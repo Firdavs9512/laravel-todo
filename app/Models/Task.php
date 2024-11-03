87857b0d54d6ade25a6da5c14ee2ff09f1e9ff09
Firdavs <firdavs20fe@gmail.com>
2024-11-03 20:40:36 +0300

    feat: add database table task

---
+++ app/Models/Task.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Task extends Model
+{
+    /** @use HasFactory<\Database\Factories\TaskFactory> */
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'title',
+        'description',
+        'is_completed',
+        'user_id'
+    ];
+
+    /**
+     * Get the user that owns the task.
+     *
+     * @return BelongsTo<User>
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
